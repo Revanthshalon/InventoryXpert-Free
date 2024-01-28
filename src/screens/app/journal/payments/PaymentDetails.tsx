c6ac87abc838258359e7a8159a4a609328f90956
S Revanth Shalon Raj <revanthshalon@gmail.com>
2024-01-28 11:50:28 +0530

    Add PaymentDetails and PurchaseDetails components

---
+++ src/screens/app/journal/payments/PaymentDetails.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const PaymentDetails = (props: Props) => {
+  return (
+    <View>
+      <Text>PaymentDetails</Text>
+    </View>
+  );
+};
+
+export default PaymentDetails;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/app/journal/purchases/PurchaseDetails.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const PurchaseDetails = (props: Props) => {
+  return (
+    <View>
+      <Text>PurchaseDetails</Text>
+    </View>
+  )
+}
+
+export default PurchaseDetails
+
+const styles = StyleSheet.create({})
